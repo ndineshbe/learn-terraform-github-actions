b428c0ff3eefb48857e61c7253c5e423e6fe6d08
DINESH-PC\Dinesh <dnagarajan@guidewire.com>
2021-12-15 17:41:25 +0530

    point backend to correct TFC organization and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "myLearning-acg"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-tf-github"
     }
   }
 }
